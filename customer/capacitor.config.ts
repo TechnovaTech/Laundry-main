ac599baee2849ddff6c0f2189b222704dce4c2b9
Yash Kathiriya <yashkathiriya370@gmail.com>
2025-12-11 14:56:08 +0530

    customer app fix

--- customer/capacitor.config.ts
+++ customer/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.urbansteam.customer',
+  appId: 'com.urbansteam.customerapp',
   appName: 'Urban Steam',
   webDir: 'dist',
   server: {
